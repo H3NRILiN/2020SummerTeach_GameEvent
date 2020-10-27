b4a008ffee0173434842dfddff8ecc6ac995ab1c
H3NRILiN <so7788520@gmail.com>
2020-10-27 14:57:36 +0800

    2020_10_26

--- Assets/_Lessons/4_GameEvent/ScriptableObject Event/AreaInteractObject.cs
+++ Assets/_Lessons/4_GameEvent/ScriptableObject Event/AreaInteractObject.cs
@@ -6,35 +6,39 @@ public class AreaInteractObject : MonoBehaviour
 {
     #region 物件集合
     static Dictionary<int, AreaInteractObject> m_Collection;
-    public static AreaInteractObject GetObject(Transform transform)
+    public static AreaInteractObject GetObject(Transform _transform)
     {
-        if (!transform || !m_Collection.ContainsKey(transform.GetInstanceID()))
+        if (!_transform || !m_Collection.ContainsKey(_transform.GetInstanceID()))
             return null;
-        return m_Collection[transform.GetInstanceID()];
+        return m_Collection[_transform.GetInstanceID()];
     }
     private void OnEnable()
     {
         if (m_Collection == null)
             m_Collection = new Dictionary<int, AreaInteractObject>();
 
-        m_Collection.Add(transform.GetInstanceID(), this);
+        m_Collection.Add(m_InstanceID, this);
     }
     private void OnDisable()
     {
-        m_Collection.Remove(transform.GetInstanceID());
+        m_Collection.Remove(m_InstanceID);
     }
     #endregion
 
+    public bool m_UsingID;
+    public string m_MatchID;
     public AreaInteractionState m_StateCanInteract;
-    // Start is called before the first frame update
-    void Start()
-    {
 
+    public int m_InstanceID { get => transform.GetInstanceID(); }
+
+    public void Interact(AreaInteractObject last)
+    {
+        Debug.Log($"目前物件: {m_MatchID} | 結合物件: {(last == null ? "無" : last.m_MatchID) }");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UnInteract()
     {
-
+        Debug.Log("取消互動");
     }
+
 }
